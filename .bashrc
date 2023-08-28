7881181fb3b8ac4a5dd59e5d8646e62d321c7cf8
zdmiller12 <zdmiller12@gmail.com>
2023-08-27 20:24:18 -0400

    tweaks

--- .bashrc
+++ .bashrc
@@ -140,7 +140,6 @@ fi
 
 export JAVA_HOME=/usr/lib/jvm/default-java
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 export AWS_DEFAULT_REGION=us-east-1
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 set -g -x fish_greeting 'Do you like fish sticks?'
 
 source ~/git/config/personal_aliases.sh
-export AWS_DEFAULT_REGION=us-east-1
-export JAVA_HOME=/usr/lib/jvm/default-java
+set AWS_DEFAULT_REGION us-east-1
+set JAVA_HOME /usr/lib/jvm/default-java
+set PYENV_ROOT "$HOME/.pyenv"
+command -v pyenv >/dev/null || set PATH "$PYENV_ROOT/bin:$PATH"
 
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
