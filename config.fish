56dfcc5b2d80dcf4861c13afaee0618850be17df
zdmiller12 <zdmiller12@gmail.com>
2026-04-07 22:13:07 -0400

    asdf fish config

--- config.fish
+++ config.fish
@@ -29,6 +29,20 @@ fish_add_path --path "$PYENV_ROOT/bin"
 
 pyenv init - fish | source
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 # aliases
 
 alias gitclean="git branch | grep -v \* | xargs git branch -D"
